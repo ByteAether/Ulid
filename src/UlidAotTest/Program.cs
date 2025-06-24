f02a398b457955ab3f0de11e9e6f9eba913c6504
Joonatan Uusväli <joonatanu@gmail.com>
2025-06-24 13:35:56 +0300

    Making project AoT compatible. Adding AoT console application for build test.

---
+++ src/UlidAotTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using ByteAether.Ulid;
+
+var serializerOptions = new JsonSerializerOptions
+{
+};
+serializerOptions.TypeInfoResolverChain.Add(UlidJsonContext.Default);
+
+var ulid = Ulid.New();
+Console.WriteLine($"Generated ULID: {ulid}");
+
+// Test JSON serialization
+var json = JsonSerializer.Serialize(new List<Ulid>() { ulid }, serializerOptions);
+Console.WriteLine($"Serialized to JSON: {json}");
+var deserializedUlid = JsonSerializer.Deserialize<List<Ulid>>(json, serializerOptions);
+Console.WriteLine($"Deserialized from JSON: {deserializedUlid!.First()}");
+
+Console.WriteLine($"ULIDs are equal: {ulid == deserializedUlid!.First()}");
---
+++ src/UlidAotTest/UlidJsonContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ByteAether.Ulid;
+
+[JsonSerializable(typeof(Ulid))]
+[JsonSerializable(typeof(List<Ulid>))]
+public partial class UlidJsonContext : JsonSerializerContext
+{ }
